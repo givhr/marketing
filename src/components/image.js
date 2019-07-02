86af5262b6ffea7993367754d3c4fc9bf56ff5b3
Mikayda Mills <mills.ma12@gmail.com>
2019-07-02 12:25:39 -0400

    structure to Image

--- src/components/image.js
+++ src/components/image.js
@@ -13,20 +13,25 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+const Image = ({ children }) => {
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+  //       childImageSharp {
+  //         fluid(maxWidth: 300) {
+  //           ...GatsbyImageSharpFluid
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+  // return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <ImageWrapper>{children}</ImageWrapper>
 }
 
 export default Image
+
+export const ImageWrapper = styled.div`
+  width: 100%;
+  display: flex;
+`
