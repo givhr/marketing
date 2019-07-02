5c924a09feb60878a1f394f44cf783123779f6a4
Mikayda Mills <mills.ma12@gmail.com>
2019-07-01 21:37:44 -0400

    add styled components plugin, add gorilla svg and change gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Givhr`,
+    description: `Automate giveaways and surveys in Slack`,
+    author: `@givhr`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,18 +13,20 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `givhr-manifest`,
+        short_name: `givhr-gorilla`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gorilla_icon.svg`, // This path is relative to the root of the site.
+        crossOrigin: `use-credentials`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
